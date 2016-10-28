b6097cbb62b431329a5e26fb9e2f2718c09c608b
Yuliya Astapova <ellemandora@gmail.com>
2016-10-28 12:04:59 -0400

    moved scripts and css into separate files

---
+++ draw_histograms.js
@@ -0,0 +1,120 @@
+function drawAll() {
+    var heightScale = d3.scale.linear()
+        .domain([0,d3.max(dataArray.map(function(d) {return d.length}))])
+        .range([0,height-pady*2]);
+
+    var yScale = d3.scale.linear()
+        .domain([0,d3.max(dataArray.map(function(d) {return d.length}))])
+        .range([height-pady*2,0]);
+
+    var colorScale = d3.scale.linear()
+        .domain([0,d3.max(dataArray.map(function(d) {return d.x}))])
+        .range(["skyblue", "limegreen"]);
+
+    var xPos = d3.scale.linear()
+        .domain([0,d3.max(dataArray.map(function(d) {return d.x}))])
+        .range([padx/2,width-padx]);
+
+    var xScale = d3.scale.linear()
+        .domain([0,d3.max(dataArray.map(function(d) {return d.x}))])
+        .range([padx/2,width-(padx/2)]);
+
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom")
+        .ticks(bins);
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left");
+
+    var bars = canvas.selectAll("rect")
+        .data(dataArray);
+
+    var tip = d3.tip()
+        .attr("class","tooltip")
+        .offset([-10, 0])
+        .html(function(d) {
+            return "Frequency: <span style='color:pink'>" + d.length + "</span>";
+        })
+
+    canvas.call(tip);
+    bars.enter()
+        .append("rect")
+        .attr("class","bar")
+        // .attr("fill", "limegreen")
+        .attr("fill", function(d,i) { return colorScale(d.x) })
+        .attr("x", function(d) { return xPos(d.x)+10 })
+        .attr("height", 0)
+        .attr("y", function(d,i) { return height-pady })
+        .attr("width", barWidth)
+        .attr("transform", "translate(5,0)")
+        .transition()
+        .duration(800)
+        .delay(function (d,i) { return i*50 })
+            .attr("y", function(d) { return yScale(d.y) +pady})
+            .attr("height", function(d) { return heightScale(d.y) });
+
+    bars.on("mouseover", function(d) {
+            d3.select(this)
+                .attr("fill", "pink")
+                .attr("height", function(d) { return heightScale(d.y)+10 })
+                .attr("width", barWidth+10)
+                .attr("transform", "translate(0,-10)");
+            tip.show(d);
+            
+        })
+        .on("mouseout", function(d,i) {
+            d3.select(this)
+                // .attr("fill", "limegreen")
+                .attr("fill", function(d, i) { return colorScale(d.x) })
+                .attr("height", function(d) { return heightScale(d.y) })
+                .attr("width", barWidth)
+                .attr("transform", "translate(5, 0)");
+            tip.hide(d);
+        });
+
+    canvas.append("g")
+        .attr("transform", "translate(0, "+ (height - pady + 10) +")")
+        .attr("class","axis")
+        .call(xAxis);
+
+    canvas.append("g")
+        .attr("transform", "translate("+padx/2+", "+pady+")")
+        .attr("class","axis")
+        .call(yAxis);
+
+    canvas.append("text")
+        .attr("text-anchor","middle")
+        .attr("transform","translate("+padx/2+","+height/2+")rotate(-90)")
+        .attr("font-family", "Tahoma,Arial,sans-serif")
+        .text("Frequency");
+
+    canvas.append("text")
+        .attr("text-anchor","middle")
+        .attr("transform","translate("+width/2+","+(height+pady/2)+")")
+        .attr("font-family", "Tahoma,Arial,sans-serif")
+        .text("Value");
+
+    d3.select("div").on("click", function() {
+        var sel = d3.select("select").property("value");
+        var i = cols.indexOf(sel);
+        i = (i+1) % cols.length;
+        d3.select("select").property("value", cols[i]);
+        createHist(cols[i]);
+    })
+}
+
+function createHist(col) {
+    var i = cols.indexOf(col);
+    var map = allData.map(function(d) {return parseFloat(d[col]);});
+
+    var hist = d3.layout.histogram()
+        .bins(bins)
+        (map);
+    dataArray = hist;
+    canvas.selectAll("*").remove();
+    drawAll();
+    d3.select("h2")
+        .text(colnames[i]);
+}
---
+++ run_vis.js
@@ -0,0 +1,45 @@
+var allData;
+var colnames = ["US State", 
+                "Percentage of Households with Retirement Income",
+                "Per Capita Income for People with 'Other' Heritage",
+                "Percentage of Population Who Immigrated Within the Last 3 Years",
+                "Percentage of Population Who Immigrated Within the Last 5 Years",
+                "Percentage of Population Who Immigrated Within the Last 8 Years",
+                "Percentage of Population Who Immigrated Within the Last 10 Years",
+                "Owner Occupied Housing - Lower Quartile",
+                "Owner Occupied Housing - Median",
+                "Owner Occupied Housing - Upper Quartile",
+                "Rental Housing - Upper Quartile Rent",
+                "Median Owners Cost as Percentage of Household Income (With Mortgage)",
+                "Percentage of Foreign Born People",
+                "Percentage of People Born in Same State as Currently Living"];
+var dataArray = [10,20,40,60,80,100];
+var width = 800;
+var height = 800;
+var barWidth = 50;
+var bins = 10;
+var pady = 50;
+var padx = 100;
+var cols;
+
+var canvas = d3.select("div")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate("+20+", 0)");
+
+
+d3.csv("communities_crime_pca.csv", function(data) {
+    allData = data;
+    cols = Object.keys(d3.values(data)[0]);
+    console.log(cols);
+
+    d3.select("select").selectAll("option")
+        .data(cols)
+        .enter()
+        .append("option")
+        .attr("value", function(d) {return d})
+        .text(function(d) {return d});
+    createHist(cols[0]);
+});
